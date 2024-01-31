2e8021dc444da0e61a5c93947020478f0825ec82
eunielis5 <seunielis@gmail.com>
2024-01-30 20:27:54 -0400

    finished 7-12

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,2 @@
-#!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
-
+    return 97 <= ord(c) <= 122
---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def get_and_print_last_digit(number):
+    if number < 0:
+        last_digit = number % -(10)
+        print(-(last_digit), end='')
+    else:
+        last_digit = number % 10
+        print(last_digit, end='')
+    return abs(last_digit)
