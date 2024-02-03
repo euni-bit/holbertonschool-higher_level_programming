cfc37b2b22c5b1721f55498c8c1c4a5e2938adb1
eunielis5 <seunielis@gmail.com>
2024-02-02 22:45:09 -0500

    safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     num = 0
     try:
-        for item in range (x) :
+        for item in range(x):
             print("{}".format(my_list[item]), end='')
             num += 1
     except IndexError:
