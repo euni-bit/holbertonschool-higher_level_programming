0337727054e15b8044f583cc4f7bb75384114500
euni-bit <seunielis@gmail.com>
2024-05-24 16:13:51 -0400

    task 01

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,36 @@
+# task_01_duck_typing.py
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+    
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+    
+class Ractangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
