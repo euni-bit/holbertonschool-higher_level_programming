8c7ae94888d048693245042227b51c9fb9ff4476
euni-bit <seunielis@gmail.com>
2024-05-29 23:49:09 -0400

    task 3

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+def deserialize_from_xml(filename):
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        data_dict = {}
+        for child in root:
+            data_dict[child.tag] = child.text
+        return data_dict
+    except FileNotFoundError:
+        print("File not found.")
+        return None
+    except ET.ParseError as e:
+        print("Error occurred while parsing XML:", e)
+        return None
